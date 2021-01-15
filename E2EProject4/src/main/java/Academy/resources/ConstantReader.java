71a0c0b900e786b1f64ab94721c4bb38134b85fa
Kajal Thakur <kajal.thakur@zeuslearning.mumbai>
2021-01-15 20:35:03 +0530

    E2EProject-selenium-project

---
+++ E2EProject4/src/main/java/Academy/resources/ConstantReader.java
@@ -0,0 +1,40 @@
+package Academy.resources;
+
+public class ConstantReader {
+	private static final String CHROME_BROWSER = "chrome";
+	private static final String FIREFOX_BROWSER = "firefox";
+	private static final String HOMEPAGE_TITLE = "FEATURED COURSES123";
+	private static final String REPORT_NAME = "Web Automation Results";
+	private static final String DOCUMENT_TITLE = "Test Results";
+	private static final String TESTER_NAME = "Kajal Thakur";
+	private static final String TESTER_INFO = "Tester";
+
+	public static String getChromeBrowser() {
+		return CHROME_BROWSER;
+	}
+
+	public static String getFirefoxBrowser() {
+		return FIREFOX_BROWSER;
+	}
+
+	public static String getHomepageTitle() {
+		return HOMEPAGE_TITLE;
+	}
+
+	public static String getReportName() {
+		return REPORT_NAME;
+	}
+
+	public static String getDocumentTitle() {
+		return DOCUMENT_TITLE;
+	}
+
+	public static String getTesterName() {
+		return TESTER_NAME;
+	}
+
+	public static String getTesterInfo() {
+		return TESTER_INFO;
+	}
+
+}
